ef6fba9db6c73a91574de74176159d80850abf0a
juceliojunior <jucelio.durval@gmail.com>
2024-07-10 16:44:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
   const apiKey = 'https://v3.football.api-sports.ioc10fe51d409b29d76ef8e7396a6a8257';
-  const leagueId = 71; // ID da Série A Brasileira na API
+  const leaguesUrl = 'https://v3.football.api-sports.io/leagues';
+
+  fetch(leaguesUrl, {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': apiKey,
+      'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    const leagues = data.response;
+    const leaguesListContainer = document.getElementById('leagues-list');
+
+    leagues.forEach(league => {
+      const leagueElement = document.createElement('div');
+      leagueElement.className = 'league-item';
+      leagueElement.innerHTML = `
+        <h2>${league.league.name}</h2>
+        <p>País: ${league.country.name}</p>
+        <p>Descrição: ${league.league.description}</p>
+        <button onclick="fetchLeagueInfo(${league.league.id})">Ver Detalhes</button>
+      `;
+      leaguesListContainer.appendChild(leagueElement);
+    });
+  })
+  .catch(error => console.error('Erro ao buscar informações das ligas:', error));
+});
+
+function fetchLeagueInfo(leagueId) {
+  const apiKey = 'https://v3.football.api-sports.ioc10fe51d409b29d76ef8e7396a6a8257';
   const leagueUrl = `https://v3.football.api-sports.io/leagues?id=${leagueId}`;
 
   fetch(leagueUrl, {
@@ -12,16 +42,14 @@ document.addEventListener('DOMContentLoaded', function() {
   })
   .then(response => response.json())
   .then(data => {
-    const leagueInfo = data.response[0]; // Informações da primeira liga encontrada
+    const leagueInfo = data.response[0];
     console.log('Informações da Liga:', leagueInfo);
-    // Aqui você pode processar e exibir as informações da liga como desejar
-    const leagueContainer = document.getElementById('league-info');
-    leagueContainer.innerHTML = `
+    const leagueInfoContainer = document.getElementById('league-info');
+    leagueInfoContainer.innerHTML = `
       <h2>${leagueInfo.league.name}</h2>
       <p>País: ${leagueInfo.country.name}</p>
-      <p>Temporada atual: ${leagueInfo.seasons[0].year}</p>
       <p>Descrição: ${leagueInfo.league.description}</p>
     `;
   })
   .catch(error => console.error('Erro ao buscar informações da liga:', error));
-});
+}
