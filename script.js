f65a6154f89fb5bc15fcc86039151aa7c31f99ab
juceliojunior <jucelio.durval@gmail.com>
2024-07-10 17:01:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-body {
-  font-family: Arial, sans-serif;
-  background-color: #f0f0f0;
-  color: #333;
-  text-align: center;
-}
+document.addEventListener('DOMContentLoaded', function() {
+  const apiKey = 'https://v3.football.api-sports.ioc10fe51d409b29d76ef8e7396a6a8257';
+  const leaguesUrl = 'https://v3.football.api-sports.io/leagues';
 
-h1 {
-  color: #2c3e50;
-}
+  fetch(leaguesUrl, {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': apiKey,
+      'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    const leagues = data.response;
+    const leaguesListContainer = document.getElementById('leagues-list');
 
-#content {
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  margin-top: 20px;
-}
+    leagues.forEach(league => {
+      const leagueElement = document.createElement('div');
+      leagueElement.className = 'league-item';
+      leagueElement.innerHTML = `
+        <h2>${league.league.name}</h2>
+        <p>País: ${league.country.name}</p>
+        <p>Descrição: ${league.league.description}</p>
+        <button onclick="fetchLeagueInfo(${league.league.id})">Ver Detalhes</button>
+      `;
+      leaguesListContainer.appendChild(leagueElement);
+    });
+  })
+  .catch(error => console.error('Erro ao buscar informações das ligas:', error));
+});
 
-#leagues-list {
-  flex: 1;
-  text-align: left;
-  margin-right: 20px;
-}
+function fetchLeagueInfo(leagueId) {
+  const apiKey = 'https://v3.football.api-sports.ioc10fe51d409b29d76ef8e7396a6a8257';
+  const leagueUrl = `https://v3.football.api-sports.io/leagues?id=${leagueId}`;
 
-.league-item {
-  background: #fff;
-  margin-bottom: 10px;
-  padding: 10px;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0,0,0,0.1);
-}
-
-button {
-  background-color: #3498db;
-  color: white;
-  border: none;
-  padding: 5px 10px;
-  border-radius: 3px;
-  cursor: pointer;
-  margin-top: 10px;
-}
-
-button:hover {
-  background-color: #2980b9;
-}
-
-#league-info {
-  flex: 1;
-  text-align: left;
-  padding: 20px;
-  background: #fff;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  fetch(leagueUrl, {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': apiKey,
+      'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    const leagueInfo = data.response[0];
+    console.log('Informações da Liga:', leagueInfo);
+    const leagueInfoContainer = document.getElementById('league-info');
+    leagueInfoContainer.innerHTML = `
+      <h2>${leagueInfo.league.name}</h2>
+      <p>País: ${leagueInfo.country.name}</p>
+      <p>Descrição: ${leagueInfo.league.description}</p>
+    `;
+  })
+  .catch(error => console.error('Erro ao buscar informações da liga:', error));
 }
